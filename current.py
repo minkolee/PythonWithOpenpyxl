3777f6680b4284dd298feb5183e163de3ab3e171
minkolee <minkolee@gmail.com>
2020-05-24 14:32:04 +0800

    录制完毕

--- current.py
+++ current.py
@@ -1,75 +1,20 @@
 import openpyxl
 import decimal
 import re
+import datetime
 
 
-def transfer_to_decimal(num) -> decimal.Decimal:
-    # 如果是一个整数, 就直接进行转换
-    if type(num) == int:
-        return decimal.Decimal(num)
+ws = openpyxl.load_workbook('datatypes.xlsx').active
 
-    # 如果是一个浮点数, 将其转换成2位小数的字符串表示, 然后使用字符串来创建Decimal对象
-    elif type(num) == float:
-        return decimal.Decimal('{0:.2f}'.format(num))
+print("{} 的数据类型是 {}".format(ws['A1'].value, type(ws['A1'].value)))
+print("{} 的数据类型是 {}".format(ws['A2'].value, type(ws['A2'].value)))
+print("{} 的数据类型是 {}".format(ws['A3'].value, type(ws['A3'].value)))
+print("{} 的数据类型是 {}".format(ws['A4'].value, type(ws['A4'].value)))
+print("{} 的数据类型是 {}".format(ws['A5'].value, type(ws['A5'].value)))
 
-    # 如果是一个字符串, 需要判断其格式
-    elif type(num) == str:
+print("{} 的数据类型是 {}".format(ws['B1'].value.today(), type(ws['B1'].value)))
+print("{} 的数据类型是 {}".format(ws['B2'].value, type(ws['B2'].value)))
+print("{} 的数据类型是 {}".format(ws['B3'].value, type(ws['B3'].value)))
+print("{} 的数据类型是 {}".format(ws['B4'].value, type(ws['B4'].value)))
+print("{} 的数据类型是 {}".format(ws['B5'].value, type(ws['B5'].value)))
 
-        # 判断字符串是不是一个十进制的小数的表示
-        if re.match('[+-]?\\d+(\\.\\d+)?$', num):
-            # 字符串是一个十进制的小数表示
-            # 判断是否是整数, 如果是整数, 直接通过整数创建Decimal对象
-            if num.find('.') == -1:
-                return decimal.Decimal(num)
-
-            # 不是整数的情况下
-            else:
-                split_num = num.split('.')
-
-                if len(split_num[1]) > 2:
-                    num_string = split_num[0] + '.' + split_num[1][0:2]
-                    if int(split_num[1][2]) >= 5:
-                        if num[0] == '-':
-                            return decimal.Decimal(num_string) - decimal.Decimal('0.01')
-                        else:
-                            return decimal.Decimal(num_string) + decimal.Decimal('0.01')
-                    else:
-                        return decimal.Decimal(num_string)
-
-                elif len(split_num[1]) == 2:
-                    num_string = split_num[0] + '.' + split_num[1][0:2]
-                    return decimal.Decimal(num_string)
-
-                elif len(split_num[1]) == 1:
-                    num_string = split_num[0] + '.' + split_num[1] + '0'
-                    return decimal.Decimal(num_string)
-
-                else:
-                    raise AttributeError
-
-        else:
-            raise AttributeError
-
-    # 不是上述三种类型
-    else:
-        raise AttributeError
-
-
-if __name__ == '__main__':
-    wb = openpyxl.load_workbook('amount.xlsx')
-
-    ws = wb.active
-
-    # 先使用decimal来计算
-    sum_decimal = decimal.Decimal('0')
-    for i in range(3,61):
-        sum_decimal = sum_decimal + transfer_to_decimal(ws['C'+str(i)].value)
-
-    print(sum_decimal)
-
-    # 使用float类型来计算
-    sum_float = 0
-    for i in range(3,61):
-        sum_float = sum_float + float(ws['C'+str(i)].value)
-
-    print(sum_float)
--- tools.py
+++ tools.py
@@ -1,6 +1,8 @@
 import os
 import openpyxl
 import random
+import decimal
+import re
 
 
 # 返回一个目录内所有的.xlsx文件的路径
@@ -53,3 +55,55 @@ def monthlize(path: str):
 
     wb.remove(wb.active)
     wb.save(path.split('.')[0] + 'monthly.xlsx')
+
+
+def transfer_to_decimal(num) -> decimal.Decimal:
+    # 如果是一个整数, 就直接进行转换
+    if type(num) == int:
+        return decimal.Decimal(num)
+
+    # 如果是一个浮点数, 将其转换成2位小数的字符串表示, 然后使用字符串来创建Decimal对象
+    elif type(num) == float:
+        return decimal.Decimal('{0:.2f}'.format(num))
+
+    # 如果是一个字符串, 需要判断其格式
+    elif type(num) == str:
+
+        # 判断字符串是不是一个十进制的小数的表示
+        if re.match('[+-]?\\d+(\\.\\d+)?$', num):
+            # 字符串是一个十进制的小数表示
+            # 判断是否是整数, 如果是整数, 直接通过整数创建Decimal对象
+            if num.find('.') == -1:
+                return decimal.Decimal(num)
+
+            # 不是整数的情况下
+            else:
+                split_num = num.split('.')
+
+                if len(split_num[1]) > 2:
+                    num_string = split_num[0] + '.' + split_num[1][0:2]
+                    if int(split_num[1][2]) >= 5:
+                        if num[0] == '-':
+                            return decimal.Decimal(num_string) - decimal.Decimal('0.01')
+                        else:
+                            return decimal.Decimal(num_string) + decimal.Decimal('0.01')
+                    else:
+                        return decimal.Decimal(num_string)
+
+                elif len(split_num[1]) == 2:
+                    num_string = split_num[0] + '.' + split_num[1][0:2]
+                    return decimal.Decimal(num_string)
+
+                elif len(split_num[1]) == 1:
+                    num_string = split_num[0] + '.' + split_num[1] + '0'
+                    return decimal.Decimal(num_string)
+
+                else:
+                    raise AttributeError
+
+        else:
+            raise AttributeError
+
+    # 不是上述三种类型
+    else:
+        raise AttributeError
