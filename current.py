5bd698a0670284822c689276b0c88ba9a22bb4cb
minkolee <minkolee@gmail.com>
2020-07-01 12:21:21 +0800

    新视频准备好了

--- current.py
+++ current.py
@@ -2,61 +2,70 @@ import openpyxl
 import tools
 
 
-# 将worksheet中index列中的内容按照指定的分隔符分割, 并将结果分割成number列
-def text_to_columns(index: int, delimiter: str, number: int, worksheet):
-    # 首先确定填充范围, 也是从2-max_rows-1
-    max_row = ws.max_row
+def subtotal_single(key_column:int, value_column:int, file_name:str)->dict:
+    wb = tools.open_xlsx_file(file_name)
 
-    # 然后需要准备空行, 注意将结果分割成number列, 即在index列后插入number-1列, 所以先插入列
-    worksheet.insert_cols(index + 1, number - 1)
+    result = {}
 
-    # 然后把列头填充一下, 从index 填充到index+number-1, 一共 number列, 这里因为是range所以正好替我们减了1
-    for i in range(index, index + number):
-        ws.cell(row=1, column=i, value=str(i - index + 1) + '级科目')
+    # 2 -> wb.max_row -1 是要处理的列
+    for i in range(2, wb.max_row):
 
-    # 之后每一行进行分割, 然后向同一行填充, 这里注意如果分割数量多, 需要控制不要填充出number的范围
-    for i in range(2, max_row):
-        # 分割index列的单元格的字符串
-        split_cell = ws.cell(row=i, column=index).value.split(delimiter)
+        # 键是否存在于字典中
+        key = wb.cell(row=i,column=key_column).value
 
-        # 如果分割的长度大于number, 只填充到number为止
-        if len(split_cell) > number:
-            # 从(i, index+j)的单元格开始横向填充number数量的三个单元格
-            for j in range(0, number):
-                ws.cell(row=i, column=index + j, value=split_cell[j].strip())
-        # 如果分割的长度小于等于number, 就直接填充即可
+        if key in result:
+
+            # 存在的话, 需要更新
+            result[key] = result[key] + tools.transfer_to_decimal(wb.cell(row=i,column=value_column).value)
+
+        #不存在的话, 直接设置
         else:
-            j = 0
-            for each_content in split_cell:
-                ws.cell(row=i, column=index + j, value=each_content.strip())
-                j = j + 1
+            result[key] = tools.transfer_to_decimal(wb.cell(row=i,column=value_column).value)
 
-    return worksheet
+    return result
 
+def subtotal_composite(key_column:int, value_column1:int, value_column2:int, file_name:str) -> dict:
 
-# 有了分列函数, 再来编写最终的处理函数
-def process_worksheet(worksheet):
-    # 填充5-6列
-    tools.fill_column(5, worksheet)
-    tools.fill_column(6, worksheet)
+    wb = tools.open_xlsx_file(file_name)
 
-    # 删除列, 注意先删除右边的, 这样要删除的列号不会变化
-    worksheet.delete_cols(15, 18)
-    worksheet.delete_cols(10, 3)
-    worksheet.delete_cols(8)
-    worksheet.delete_cols(1, 4)
+    result = {}
+    # 2 -> wb.max_row -1 是要处理的列
+    for i in range(2, wb.max_row):
 
-    # 分列, 注意此时分的是第四列
-    text_to_columns(4, '-', 3, worksheet)
+        # 键是否存在于字典中
+        key = wb.cell(row=i, column=key_column).value
 
-    return worksheet
+        if key in result:
+            # 如果存在, 要更新两个值
+            result[key][wb.cell(row=1,column=value_column1).value] = result[key][wb.cell(row=1,column=value_column1).value] + tools.transfer_to_decimal(wb.cell(row=i, column=value_column1).value)
+            result[key][wb.cell(row=1,column=value_column2).value] = result[key][wb.cell(row=1,column=value_column2).value] + tools.transfer_to_decimal(wb.cell(row=i, column=value_column2).value)
+
+        # 不存在的话, 创建键和对应的嵌套字典, 初始值是0
+        else:
+            result[key] = {}
+            result[key][wb.cell(row=1,column=value_column1).value] = tools.transfer_to_decimal(wb.cell(row=i, column=value_column1).value)
+            result[key][wb.cell(row=1,column=value_column2).value] = tools.transfer_to_decimal(wb.cell(row=i, column=value_column2).value)
+
+    return result
 
 
 if __name__ == '__main__':
-    wb = openpyxl.load_workbook('kisdocument.xlsx')
 
+    result = subtotal_single(4,7,'data.xlsx')
+
+    # 为了方便, 可以写一个函数将内容输出
+
+    wb = openpyxl.Workbook()
     ws = wb.active
 
-    process_worksheet(ws)
+    ws.cell(1,1,"科目")
+    ws.cell(1,2,"借方")
 
-    ws.parent.save("result.xlsx")
+    start = 2
+
+    for(k,v) in result.items():
+        ws.cell(row=start, column=1, value=k)
+        ws.cell(row=start, column=2, value=v)
+        start = start+1
+
+    wb.save('result.xlsx')
--- tools.py
+++ tools.py
@@ -165,3 +165,52 @@ def cal_current_ratio(worksheet, config) -> str:
     total_liquid_liability = worksheet[config['ratio']['liquid_liability']].value
 
     return "{:.2f}%".format(total_liquid_asset / total_liquid_liability * 100)
+
+# 分列函数
+def text_to_columns(index: int, delimiter: str, number: int, worksheet):
+    # 首先确定填充范围, 也是从2-max_rows-1
+    max_row = ws.max_row
+
+    # 然后需要准备空行, 注意将结果分割成number列, 即在index列后插入number-1列, 所以先插入列
+    worksheet.insert_cols(index + 1, number - 1)
+
+    # 然后把列头填充一下, 从index 填充到index+number-1, 一共 number列, 这里因为是range所以正好替我们减了1
+    for i in range(index, index + number):
+        ws.cell(row=1, column=i, value=str(i - index + 1) + '级科目')
+
+    # 之后每一行进行分割, 然后向同一行填充, 这里注意如果分割数量多, 需要控制不要填充出number的范围
+    for i in range(2, max_row):
+        # 分割index列的单元格的字符串
+        split_cell = ws.cell(row=i, column=index).value.split(delimiter)
+
+        # 如果分割的长度大于number, 只填充到number为止
+        if len(split_cell) > number:
+            # 从(i, index+j)的单元格开始横向填充number数量的三个单元格
+            for j in range(0, number):
+                ws.cell(row=i, column=index + j, value=split_cell[j].strip())
+        # 如果分割的长度小于等于number, 就直接填充即可
+        else:
+            j = 0
+            for each_content in split_cell:
+                ws.cell(row=i, column=index + j, value=each_content.strip())
+                j = j + 1
+
+    return worksheet
+
+
+# 固定处理序时账的函数
+def process_worksheet(worksheet):
+    # 填充5-6列
+    tools.fill_column(5, worksheet)
+    tools.fill_column(6, worksheet)
+
+    # 删除列, 注意先删除右边的, 这样要删除的列号不会变化
+    worksheet.delete_cols(15, 18)
+    worksheet.delete_cols(10, 3)
+    worksheet.delete_cols(8)
+    worksheet.delete_cols(1, 4)
+
+    # 分列, 注意此时分的是第四列
+    text_to_columns(4, '-', 3, worksheet)
+
+    return worksheet
