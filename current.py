db1e6d4247796e307580d3edb9f8fe04654558ef
minkolee <minkolee@gmail.com>
2020-07-20 19:36:25 +0800

    更新一下视频文件

--- current.py
+++ current.py
@@ -1,3 +1,24 @@
 import openpyxl
 import tools
 
+ws = tools.open_xlsx_file('origin.xlsx')
+
+ws.merge_cells('D9:E10')
+
+ws.parent.save('merged.xlsx')
+
+ws = tools.open_xlsx_file('merged.xlsx')
+
+print(ws.cell(row=9, column=4).value)
+print(ws.cell(row=9, column=5).value)
+print(ws.cell(row=10, column=4).value)
+print(ws.cell(row=10, column=5).value)
+print("---------------------------------")
+print(type(ws.cell(row=9, column=4).value))
+print(type(ws.cell(row=9, column=5).value))
+print(type(ws.cell(row=10, column=4).value))
+print(type(ws.cell(row=10, column=5).value))
+
+ws.unmerge_cells('D9:E10')
+
+ws.parent.save("unmerged.xlsx")
